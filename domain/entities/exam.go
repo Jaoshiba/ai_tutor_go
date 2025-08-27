f0ff15840d03f9e2c6216150654a55bed14f3880
Jaoshiba <blackspell909@gmail.com>
2025-08-27 14:57:26 +0700

    exam ini

--- domain/entities/exam.go
+++ domain/entities/exam.go
@@ -10,3 +10,10 @@ type ExamDataModel struct {
 	CreatedAt   time.Time `json:"createdAt" db:"createdAt,omitempty"`
 	UpdatedAt   time.Time `json:"updatedAt" db:"updatedAt,omitempty"`
 }
+
+type ExamRequest struct {
+	ModuleId    string `json:"module" db:"module,omitempty"`
+	Content     string `json:"content" db:"content,omitempty"`
+	Difficulty  string `json:"difficulty" db:"difficulty,omitempty"`
+	QuestionNum int    `json:"questionNum" db:"questionNum,omitempty"`
+}
